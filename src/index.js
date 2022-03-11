d258ab3402def9efc00f1acb123d730341ab1acb
Esteban Palomeque <epalomeque@grupocentrico.com>
2022-03-11 14:02:55 -0500

    Update vue loader version

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { HelloWorld } from './components/HelloWorld'
+export { Button } from './stories/Button.vue'
+export { Header } from './stories/Header.vue'
+export { Page } from './stories/Page'
